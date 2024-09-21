df38805a7e58406614277cc9088dc148274155ee
Sergio Badke <133900607+SergioBadke@users.noreply.github.com>
2024-09-21 15:08:04 -0300

    Update cartas.php

--- cartas.php
+++ cartas.php
@@ -79,38 +79,36 @@ for ($i = 0; $i < 10; $i++) {
 
 $sorteado = $cartas[array_rand($cartas)];
 
-//print ($sorteado);
+// print ($sorteado);
 
 $escolhaNum=0;
 $escolhaNome=0;
 
-$errado=false;
 print "---Instruções---\n";
 print("1-Descubra o numero (1 -> 10) e o naipe da carta (Copas; Espadas; Ouros; Paus).\n2-Os numeros devem ser escritos em numerais (1;2;3;etc.) e o naipe em PascalCase, ou seja, a primeira letra maiuscula (Copas).\n3-Naipe e Numeros serão escritos em caixas diferentes, e o programa avisa caso você acerte alguma\n---Boa sorte e bom jogo---\n");
 //Nenhum certo\\
-while ($errado=true){
-if ($escolhaNum != $sorteado->getNumero() and $escolhaNome != $sorteado->getNome()) {
-    print("\nNenhum acerto\n");
-    $escolhaNum = readline("Número:");
-    $escolhaNome = readline("Naipe:");
-}
+while (true){
+    if ($escolhaNum != $sorteado->getNumero() and $escolhaNome != $sorteado->getNome()) {
+        print("\nNenhum acerto\n");
+        $escolhaNum = readline("Número:");
+        $escolhaNome = readline("Naipe:");
+    }
 
-//Só numero certo\\
-elseif ($escolhaNome != $sorteado->getNome()) {
-    print("\nNumero certo: ". $sorteado->getNumero()."\n");
-    $escolhaNome = readline("Naipe:");
-}
+    //Só numero certo\\
+    elseif ($escolhaNome != $sorteado->getNome()) {
+        print("\nNumero certo: ". $sorteado->getNumero()."\n");
+        $escolhaNome = readline("Naipe:");
+    }
 
-//Só Naipe certo\\
-elseif ($escolhaNum != $sorteado->getNumero()) {
-    print("\nNaipe certo: ". $sorteado->getNome() ."\n");
-    $escolhaNum = readline("Número:");
-}
+    //Só Naipe certo\\
+    elseif ($escolhaNum != $sorteado->getNumero()) {
+        print("\nNaipe certo: ". $sorteado->getNome() ."\n");
+        $escolhaNum = readline("Número:");
+    }
 
-else{
-    print("Parabens, você acertou!\nNúmero sorteado:".$sorteado);
-    $errado=false;
-    die();
-}
+    else{
+        print("Parabens, você acertou!\nNúmero sorteado:".$sorteado);
+        die();
+    }
 
 }
